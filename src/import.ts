5d3636589529fe3d36f4ef4546dbd82b03bca312
Nicolau <17990891+nicxlau@users.noreply.github.com>
2026-05-18 15:46:49 -0300

    improv `IMPORT()`

--- src/import.ts
+++ src/import.ts
@@ -1,6 +1,22 @@
-export async function IMPORT(path: string) {
+export async function IMPORT(path: string, name: string): Promise<unknown>
+export async function IMPORT(path: string, ...name: string[]): Promise<Record<string, unknown>> {
   if (process.platform === 'win32' && !path.startsWith('file://'))
     path = 'file://' + path
 
-  return import(path + '?v=' + Date.now())
+  const source = path +'?v='+ Date.now()
+
+  if (name.length > 0) {
+    const module = await import(source)
+    const result: Record<string, any> = {}
+    for (const n of name) {
+      if (n in module)
+        result[n] = module[n]
+      else
+        throw new Error(`Module ${path} does not export ${n}`)
+    }
+
+    return name.length === 1 ? result[name[0]] : result
+  }
+
+  return import(source)
 }
